7de6e3e873420730a274ada156358b1ae4d5bbec
Marcel Blijleven <marcelblijleven@gmail.com>
2020-10-02 09:29:08 +0200

    wip: test blackbox test package

--- dsmr/p1_test.go
+++ dsmr/p1_test.go
@@ -1,75 +1,10 @@
 package dsmr
 
 import (
-	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/tarm/serial"
 	"testing"
 )
-
-type TestPortReader struct {
-	openErr       error
-	OpenWasCalled bool
-}
-
-func (t *TestPortReader) OpenPort(_ *serial.Config) (*serial.Port, error) {
-	t.OpenWasCalled = true
-
-	if t.openErr != nil {
-		return nil, t.openErr
-	}
-
-	return &serial.Port{}, nil
-}
-
-func TestOpenPort(t *testing.T) {
-	portReader := &TestPortReader{OpenWasCalled: false}
-
-	var (
-		baud   = ""
-		parity = ""
-		device = "/tty/test"
-	)
-
-	_, err := OpenPort(portReader.OpenPort, device, baud, parity)
-	assert.NoError(t, err)
-	assert.True(t, portReader.OpenWasCalled)
-}
-
-func TestOpenPort_EmptyDevice(t *testing.T) {
-	portReader := &TestPortReader{OpenWasCalled: false}
-
-	var (
-		baud   = ""
-		parity = ""
-		device = ""
-	)
-
-	_, err := OpenPort(portReader.OpenPort, device, baud, parity)
-	assert.Error(t, err)
-	assert.ErrorIs(t, ErrNoDeviceProvided, err)
-	assert.False(t, portReader.OpenWasCalled)
-}
-
-func TestOpenPort_PortReaderReturnsErr(t *testing.T) {
-	expectedErr := errors.New("error reading tty")
-
-	portReader := &TestPortReader{
-		OpenWasCalled: false,
-		openErr:       expectedErr,
-	}
-
-	var (
-		baud   = ""
-		parity = ""
-		device = "/tty/test"
-	)
-
-	_, err := OpenPort(portReader.OpenPort, device, baud, parity)
-	assert.Error(t, err)
-	assert.ErrorIs(t, expectedErr, err)
-	assert.True(t, portReader.OpenWasCalled)
-}
 
 func TestCheckParity(t *testing.T) {
 	// even mark none odd space
